65e426d914a2faa749da58747f2acfd141f748d7
Sandeep Ashok <51964832+sandeepashok@users.noreply.github.com>
2024-07-15 22:39:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import "./styles.css";
+import { FaPlay, FaStop } from "react-icons/fa6";
+import { ImCross } from "react-icons/im";
+import { RiShareForwardFill } from "react-icons/ri";
+import { getDefaultMatrix, getUpdatedMatrix } from "./actions";
+import useInterval from "@use-it/interval";
+
+export default function App() {
+  const [matrix, setMatrix] = useState(getDefaultMatrix());
+  const [isRunning, setIsRunning] = useState(false);
+  const [id, setId] = useState(null);
+
+  const handleCellClick = (i, j) => {
+    let matrixCopy = [...matrix];
+    matrix[i][j] = !matrix[i][j];
+    setMatrix(matrixCopy);
+  };
+
+  const handleNext = () => {
+    setMatrix((matrix) => getUpdatedMatrix(matrix));
+  };
+
+  const handleStart = () => {
+    setIsRunning((isRunning) => !isRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    clearInterval(id);
+    setId(null);
+    setMatrix(getDefaultMatrix());
+  };
+
+  useInterval(
+    () => {
+      isRunning ? handleNext() : null;
+    },
+    isRunning ? 500 : null
+  );
+
+  return (
+    <div className="App">
+      <h1 className="heading">Conway's Game Of Life</h1>
+      <div className="game-container">
+        {matrix.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((cell, cellIndex) => (
+              <div
+                key={cellIndex}
+                className={cell ? "cell alive-cell" : "cell"}
+                onClick={() => handleCellClick(rowIndex, cellIndex)}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="button-group">
+        <button className="medium-button" onClick={handleReset}>
+          <ImCross />
+          reset
+        </button>
+        {isRunning ? (
+          <button className="large-button" onClick={handleStart}>
+            <FaStop />
+            stop
+          </button>
+        ) : (
+          <button className="large-button" onClick={handleStart}>
+            <FaPlay />
+            start
+          </button>
+        )}
+        <button className="medium-button" onClick={handleNext}>
+          <RiShareForwardFill />
+          next
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/actions.js
@@ -0,0 +1,95 @@
+const generateMatrix = () => {
+  return Array.from({ length: 30 }, () =>
+    Array.from({ length: 30 }, () => false)
+  );
+};
+
+export const getDefaultMatrix = () => {
+  let matrix = generateMatrix();
+
+  // matrix[11][5] = true;
+  // matrix[12][5] = true;
+  // matrix[13][5] = true;
+  // matrix[14][5] = true;
+  // matrix[15][5] = true;
+  // matrix[15][6] = true;
+  // matrix[15][7] = true;
+  // matrix[11][9] = true;
+  // matrix[12][9] = true;
+  // matrix[13][9] = true;
+  // matrix[14][9] = true;
+  // matrix[15][9] = true;
+  // matrix[15][10] = true;
+  // matrix[15][11] = true;
+  // matrix[14][11] = true;
+  // matrix[13][11] = true;
+  // matrix[12][11] = true;
+  // matrix[11][11] = true;
+  // matrix[11][13] = true;
+  matrix[12][13] = true;
+  // matrix[13][13] = true;
+  // matrix[14][13] = true;
+  // matrix[15][13] = true;
+  // matrix[11][14] = true;
+  // matrix[11][15] = true;
+  // matrix[13][14] = true;
+  // matrix[13][15] = true;
+  // matrix[11][17] = true;
+  // matrix[12][17] = true;
+  // matrix[13][17] = true;
+  // matrix[14][17] = true;
+  // matrix[15][17] = true;
+  // matrix[11][18] = true;
+  // matrix[11][19] = true;
+  // matrix[13][18] = true;
+  // matrix[13][19] = true;
+  // matrix[11][21] = true;
+  // matrix[12][21] = true;
+  // matrix[13][21] = true;
+  // matrix[13][22] = true;
+  // matrix[11][23] = true;
+  // matrix[12][23] = true;
+  // matrix[13][23] = true;
+  // matrix[14][23] = true;
+  // matrix[15][23] = true;
+  // matrix[15][22] = true;
+  // matrix[15][21] = true;
+  return matrix;
+};
+
+export const getUpdatedMatrix = (prevMatrix) => {
+  const updatedMatrix = generateMatrix();
+
+  prevMatrix.forEach((currRow, i) => {
+    currRow.forEach((cell, j) => {
+      const neighbouringCells = [
+        [i, j - 1],
+        [i, j + 1],
+        [i - 1, j],
+        [i + 1, j],
+        [i - 1, j - 1],
+        [i - 1, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j + 1],
+      ];
+
+      let totalNeighboursAlive = 0;
+
+      neighbouringCells.forEach((cellNum) => {
+        const [row, cell] = cellNum;
+        prevMatrix[row] && prevMatrix[row][cell]
+          ? totalNeighboursAlive++
+          : null;
+      });
+
+      cell
+        ? totalNeighboursAlive === 2 || totalNeighboursAlive === 3
+          ? (updatedMatrix[i][j] = true)
+          : (updatedMatrix[i][j] = false)
+        : totalNeighboursAlive === 3
+        ? (updatedMatrix[i][j] = true)
+        : null;
+    });
+  });
+  return updatedMatrix;
+};
